ccd77912d324b1138c8491a103dd303d2e81bb3d
nguyenlam <lthanhnguyen951@gmail.com>
2025-09-10 10:38:37 +0700

    Add files via upload

---
+++ eventsphere-backend/src/main/java/com/eventsphere/EventsphereApplication.java
@@ -0,0 +1,11 @@
+package com.eventsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsphereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventsphereApplication.class, args);
+    }
+}
